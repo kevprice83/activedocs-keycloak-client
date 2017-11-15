8dc454fb021ad100d41f42f20cf96320416905e3
Kevin Price <kev.price83@gmail.com>
2017-11-15 18:29:54 +0100

    adds JS auth client & html form

---
+++ auth.js
@@ -0,0 +1,67 @@
+var str = '{{auth_url}}',
+  delimiter = '/auth',
+  start = 2,
+  fragments = str.split(delimiter).slice(0,start),
+  result = fragments.join(delimiter);
+  var clientId;
+  var clientSecret;
+  
+  function setCredentials(clientId, clientSecret, element) {
+    element.setAttribute("onclick", "getToken(clientId, clientSecret);")
+    
+  }
+  
+  function addKey(call) {
+    var sel = document.getElementById('keySelect');
+    var el = document.getElementById('oidc');
+    keys = sel.options[sel.selectedIndex].value;
+  	clientSecret = keys.split("|")[0]
+    clientId = keys.split("|")[1]
+    
+    if (call == 'auth') {
+    el.setAttribute("href", "{{auth_url}}?response_type=code&client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=https://hot-rox-test.3scale.net/docs&state=3scale");
+    } else if (call == 'token') {
+      setCredentials(clientId, clientSecret, el);
+    }
+}
+  function getToken(clientId, clientSecret) {
+  {% assign params = request.request_uri | split: '?' | last %}
+    console.log("params: " + '{{params}}')
+  {% if params contains 'state=3scale' %}
+  window.history.replaceState({}, document.title, "/" + "docs");
+  
+  {% assign code = params | split: 'code=' | last | split: '&' | first %}
+    var code = '{{ code }}';
+    var tokenUrl = result + '/token';
+    var  formData = "response_type=code&client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=https://hot-rox-test.3scale.net/docs&state=3scale&grant_type=authorization_code&code=" + code;  //Name value Pair
+   
+$.ajax({
+    url : tokenUrl,
+    type: "POST",
+    data : formData,
+    success: function(response, textStatus, jqXHR)
+    { 
+      	var token = response.access_token;
+      	console.log(response); //data - response from server
+    	var tokenResponse = false;
+      	var inputs = $("input[name='Authorization']");
+      	if (inputs.length > 0) {
+        	tokenResponse = true;
+        	// set the value of the inputs
+        	inputs.each(function(index) {
+          	$(this).val('Bearer '+token);
+        	});
+        	alert("Success! Token: " + token);
+        	return;
+      	}
+    },
+    error: function (response, textStatus, jqXHR)
+    {
+ 		alert(textStatus);
+    }
+});
+   
+   {% elsif params contains 'state='  %}
+   alert('state is invalid or expired');
+    {% endif %}
+    };
